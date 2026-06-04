7cb35de0043ce77e782dd1d8bf3148e5f4991684
didier-snake <didier-snake@didier-snakes-MacBook-Pro.local>
2026-06-05 00:33:02 +0100

    deploy env..3

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ import * as React from "react"
 import GuestHome from "@/components/guest-home"
 export default function Page() {
   const login = () => {
-    window.location.href = "http://localhost:8888/oauth2/authorization/google"
+    // window.location.href = "http://localhost:8888/oauth2/authorization/google"
+    window.location.href =
+      "https://juriscribebackend.didierdjakoua.site/oauth2/authorization/google"
   }
   return (
     <div>
